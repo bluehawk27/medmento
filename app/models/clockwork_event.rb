b04b320a79eb5d8efe05ef3252a15ee1d6e60557
Kevin K Chen <kevin.kang.chen@gmail.com>
2014-12-03 10:22:58 -0800

    works with clientside, dependent destroy

--- app/models/clockwork_event.rb
+++ app/models/clockwork_event.rb
@@ -1,7 +1,8 @@
 class ClockworkEvent < ActiveRecord::Base
   belongs_to :user
   belongs_to :frequency_period
-  has_many :pain_ratings
+  has_many :pain_ratings, dependent: :destroy
+
   # attr_accessible :frequency_quantity, :frequency_period_id, :at
 
   # Used by clockwork to schedule how frequently this event should be run
